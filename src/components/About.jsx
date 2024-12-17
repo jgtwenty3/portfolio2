44b4eb868366c83ff56407c06cc76c60fc8b0628
Justin <justinguerrerowork@gmail.com>
2024-12-17 11:48:48 -0500

    fixed spacing

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -36,7 +36,7 @@ const About = () => {
         title = " w<b>e</b>lc<b>o</b>me t<b>o</b> my p<b>o</b>rtf<b>o</b>li<b>o</b>"
         containerClass={"mt-5 !text-black text-center"}/>
         
-        <div className='about-subtext mt-8'>
+        <div className='about-subtext px-4 md:px-0'>
           <p>I'm a tech enthusiast with a passion for helping people.</p>
           <p>I work with modern frameworks such as Next JS, GSAP, and Three.js, weaving them into interactive experiences with user friendly interfaces. </p>
         </div>
@@ -47,7 +47,7 @@ const About = () => {
           <img
           src = "img/coder.png"
           alt = "background"
-          className='absolute left-0 top-0 size-full object-cover '
+          className='absolute left-0 top-0 size-full object-cover mb-10 '
           />
         </div>
       </div>
